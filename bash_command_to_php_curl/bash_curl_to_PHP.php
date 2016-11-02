922d78ebb85f837209fa54b91e55331debb519d6
shellus <shellus@endaosi.com>
2016-11-02 10:37:13 +0800

    up

--- bash_command_to_php_curl/bash_curl_to_PHP.php
+++ bash_command_to_php_curl/bash_curl_to_PHP.php
@@ -9,7 +9,7 @@
 require 'parse_command_line.php';
 
 $str = <<<EHO
-curl 'https://www.google.com/search?espv=2&biw=1920&bih=950&q=php+curl+%E8%B7%B3%E8%BD%AC&oq=php+curl+%E8%B7%B3%E8%BD%AC&gs_l=serp.12...0.0.1.3026453.0.0.0.0.0.0.0.0..0.0....0...1c..64.serp..0.0.0.oJxfWeTwpkY&bav=on.2,or.&bvm=bv.133700528,d.dGo&fp=25ac533fec87b54f&ion=1&tch=1&ech=1&psi=5-DkV8P6AYjx0ASnrYaoDg.1474617575870.5' -H 'Referer: https://www.google.com/' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36' --compressed
+curl http://config.pinyin.sogou.com/api/toolbox/geturl.php?h=CE7B8B67F8B608F54EC2E96E1FB77B0B&v=8.0.0.8381&r=0000_sogou_pinyin_80m
 EHO;
 
 $request_info = parse_command_line($str);
@@ -17,8 +17,22 @@ $request_info = parse_command_line($str);
 $data_headers = [];
 foreach ($request_info['options']['-H'] as $line)
 {
+    $t = explode(': ', $line);
+    if(strtolower($t[0]) === 'cookie'){
+        // cookie 不直接放进header
+
+        $cookies = [];
+        $kvarr = explode('; ', substr($line, strpos($line,': ') + 2));
+        foreach ($kvarr as $kv){
+            // 之所以用索引数组而非关联数组来储存一组cookie，是因为...
+            $cookies[]=explode('=', $kv);
+        }
+        continue;
+    }
     $data_headers[] = $line;
 }
+
+
 $url = $request_info['arguments'][0];
 
 if(key_exists('--data', $request_info['options'])){
--- bash_command_to_php_curl/code-tmpl.php
+++ bash_command_to_php_curl/code-tmpl.php
@@ -3,19 +3,34 @@
 $url = '<?=$url?>';
 
 $ch = curl_init($url);
+
+
 $headers = [
 <?php foreach ($data_headers as $header): ?>
     '<?=$header;?>',
 <?php endforeach; ?>
 ];
+
+<?php if(!empty($cookies)):?>
+$cookies = [
+<?php foreach ($cookies as $cookie): ?>
+    ['<?=$cookie[0];?>', '<?=$cookie[1];?>'],
+<?php endforeach; ?>
+];
+
+foreach ($cookies as $cookie){
+    $cs[] = implode('=', $cookie);
+}
+curl_setopt($ch, CURLOPT_COOKIE, implode('; ', $cs));
+<?php endif;?>
+
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_HEADER, 0); // 不返回headers
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // 不直接输出
 curl_setopt($ch, CURLOPT_ENCODING, 'gzip'); // 自适应gzip
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // 跟随跳转
 
-
-<?php if(key_exists('--data', $request_info['options'])):?>
+<?php if(!empty($form_data)):?>
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
     <?php foreach ($form_data as $key => $value): ?>
@@ -27,7 +42,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
 $body = curl_exec($ch);
 
 if(curl_errno($ch)){
-throw new Exception(curl_error($ch));
+    throw new Exception(curl_error($ch));
 }
 
 curl_close($ch);
